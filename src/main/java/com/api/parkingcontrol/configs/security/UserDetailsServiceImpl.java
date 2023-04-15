e7770b6d3e8f03e4393ef78453653c8f6d5c594b
Fabio Martins <kiritosannee@gmail.com>
2023-04-15 16:25:05 -0300

    Added configurations of Spring Security 5.0 with components and ended the WebSecurityConfig. Now the system needs authentication in all http methods with the specific role.

--- src/main/java/com/api/parkingcontrol/configs/security/UserDetailsServiceImpl.java
+++ src/main/java/com/api/parkingcontrol/configs/security/UserDetailsServiceImpl.java
@@ -2,12 +2,16 @@ package com.api.parkingcontrol.configs.security;
 
 import com.api.parkingcontrol.models.UserModel;
 import com.api.parkingcontrol.repositories.UserRepository;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
+
 @Service
+@Transactional
 public class UserDetailsServiceImpl implements UserDetailsService {
 
     final UserRepository userRepository;
@@ -19,7 +23,8 @@ public class UserDetailsServiceImpl implements UserDetailsService {
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         UserModel userModel = userRepository.findByUsername(username)
-                .orElseThrow( () -> new UsernameNotFoundException("User Not Found with username" + username));
-        return userModel;
+                .orElseThrow(() -> new UsernameNotFoundException("User Not Found with username" + username));
+        return new User(userModel.getUsername(), userModel.getPassword(), true, true, true, true, userModel.getAuthorities());
     }
 }
+
